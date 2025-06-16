5af3258a7fd3d7105db0cfbe1e02e547db8e316f
Gaurav Bhattacharjee <144083709+0xgh057r3c0n@users.noreply.github.com>
2025-06-16 18:52:18 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Penetration Testing", "Ethical Hacking", "Vulnerability Assessment", "Web Application Security", "Network Security" , "Android Application Security"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
